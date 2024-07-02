e4cadd489e6b579bb75f6d95ed83ead3ea34a8a9
Wanga Mulaudzi <47391429+wangamulaudzi@users.noreply.github.com>
2024-07-02 03:37:28 +0200

    The start of the fake detector's frontend

--- fake-detector.py
+++ fake-detector.py
@@ -3,33 +3,77 @@
 # June 2024
 
 # Import statements
+import gcsfs # For accessing GCP storage
+import io # For reading bytes
+from PIL import Image # For opening images
 import streamlit as st # For the frontend
-from st_files_connection import FilesConnection
+from st_files_connection import FilesConnection # For creating a connection object
+
+#############
+# Variables #
+#############
+bg_img_path = "fake-detector/images/fake-detector-bg.jpeg" # Path to background image on GCP
 
 ######################
 # For the background #
 ######################
 
-# # Create connection object and retrieve file contents.
-# # Specify input format is a csv and to cache the result for 600 seconds.
-# conn = st.connection('gcs', type=FilesConnection)
-# df = conn.read("streamlit-bucket/fake-detector/images/fake-detector-bg.jpeg", input_format="csv", ttl=600)
+# Retrieve GCS secrets from Streamlit Secrets Manager
+project_id = st.secrets["connections"]["gcs"]["project_id"]
+client_email = st.secrets["connections"]["gcs"]["client_email"]
+private_key = st.secrets["connections"]["gcs"]["private_key"].replace('\\n', '\n')  # Correct formatting of the private key
 
-# Set the background
-# page_bg_img = '''
-# <style>
-# body {
-# background-image: url("https://storage.cloud.google.com/fake-detector/images/fake-detector-bg.jpeg");
-# background-size: cover;
-# }
-# </style>
-# '''
+# Initialize a GCS filesystem
+fs = gcsfs.GCSFileSystem(project=project_id, token=f'{" ".join([client_email, private_key])}')
 
-# st.markdown(page_bg_img, unsafe_allow_html=True)
+# Construct the public URL to the image in GCS
+bg_image_url = f"https://storage.cloud.google.com/{bg_img_path}"
 
-# Let the user upload a file
-# Variable to store uploaded images
-uploaded_images = st.file_uploader("Upload your image:", accept_multiple_files=True, type=["png", "jpg"])
+# Set the background style using CSS
+st.markdown(f"""
+    <style>
+    .stApp {{
+        background-image: url('{bg_image_url}');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    .stApp > header {{
+        background-color: transparent;
+    }}
+    .main .block-container {{
+        background-color: rgba(236, 224, 209, 0.9);
+        padding: 2rem;
+        border-radius: 10px;
+    }}
+    </style>
+""", unsafe_allow_html=True)
 
-for uploaded_file in uploaded_images:
-    bytes_data = uploaded_file.read()
+# Create a container for the main content
+content_container = st.container()
+
+with content_container:
+    #########
+    # Title #
+    #########
+
+    st.title("Fake Detector")
+    st.markdown("## Can AI be the judge?")
+
+    ########################
+    # Handle image uploads #
+    ########################
+
+    # Let the user upload a file
+    # Variable to store uploaded images
+    uploaded_image = st.file_uploader("Upload your image (png/jpg):", accept_multiple_files=False,
+                                       type=["png", "jpg"])
+
+    #############################
+    # Handle the uploaded_image #
+    #############################
+    if uploaded_image:
+        bytes_data = uploaded_image.read()
+
+        print(bytes_data)
